f9e254cf669ae6fc9b59a1d4aeb842d5caaa6ce2
Csávás <cszola23@hotmail.com>
2020-04-01 19:00:22 +0200

    Zolika voltam

--- WebShop/WebShop/Services/DatabaseServiceBase.cs
+++ WebShop/WebShop/Services/DatabaseServiceBase.cs
@@ -9,7 +9,7 @@ namespace WebShop.Services.DatabaseServices
 {
     public class DatabaseServiceBase
     {
-        protected readonly string serverName = "DESKTOP-IRH9O0P";
+        protected readonly string serverName = "DESKTOP-DIJF3QE";
         protected readonly string databaseName = "WebShop";
 
 
